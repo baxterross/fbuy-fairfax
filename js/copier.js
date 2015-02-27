7ef5cd00d61debcf8b56f482c4e482bddab3ac54
Baxter Ross <baxterross@gmail.com>
2015-02-27 15:24:32 -0800

    Refactoring attribute addition and overlay positioning

--- js/copier.js
+++ js/copier.js
@@ -6,8 +6,8 @@
     this.init = function(config) {
       this.button = config.button;
       var value = this.button.parentNode.getElementsByTagName('input')[0].value,
-          height = this.button.offsetHeight,
-          width = this.button.offsetWidth,
+          height = this.button.offsetHeight || 34,
+          width = this.button.offsetWidth || 81,
           scale = 2.75;
 
       this.clippy = this.createClippyObject({
@@ -26,61 +26,63 @@
     }.bind(this);
 
     this.createClippyObject = function(config) {
-      var height = config.height || 14;
-      var width = config.width || 110;
-      var bottom = config.bottom || 0;
-      var right = config.right || 0;
-      var objectAttributes = [
-        ['id', 'clippy'],
-        ['classid', 'clsid:d27cdb6e-ae6d-11cf-96b8-444553540000'],
-        ['width', width],
-        ['height', height],
-        ['style', 'opacity: 0; transform: scaleX('+(config.scale || 1)+'); position: absolute; height: '+height+'px; width: '+width+'px; bottom: '+bottom+'px; right: '+right+'px;']
-      ];
-      var object = window.document.createElement('object');
-      for (var i = 0; i < objectAttributes.length; i++) {
-        var key = objectAttributes[i][0];
-        var value = objectAttributes[i][1];
-        object.setAttribute(key, value);
+      var height = config.height || 34,
+          width = config.width || 81,
+          bottom = config.bottom || 0,
+          right = config.right || 0,
+          scale = config.scale || 1,
+          object,
+          objectAttributes = [
+            ['id', 'clippy'],
+            ['classid', 'clsid:d27cdb6e-ae6d-11cf-96b8-444553540000'],
+            ['width', width],
+            ['height', height],
+            ['style', 'opacity: 0; transform: scaleX('+scale+'); position: absolute; height: '+height+'px; width: '+width+'px; bottom: '+bottom+'px; right: '+right+'px;']
+          ],
+          param,
+          params = [
+            ['movie', '/js/clippy.swf'],
+            ['allowScriptAccess', 'always'],
+            ['quality', 'high'],
+            ['scale', 'noscale'],
+            ['FlashVars', 'text='+config.data],
+            ['bgcolor', 'transparent'],
+            ['wmode', 'transparent']
+          ],
+          embed,
+          embedAttributes = [
+            ['src', '/js/clippy.swf'],
+            ['width', width],
+            ['height', height],
+            ['name', 'clippy'],
+            ['quality', 'high'],
+            ['allowScriptAccess', 'always'],
+            ['type', 'application/x-shockwave-flash'],
+            ['pluginspage', 'http://www.macromedia.com/go/getflashplayer'],
+            ['FlashVars', 'text='+config.data],
+            ['bgcolor', 'transparent'],
+            ['wmode', 'transparent']
+          ],
+          i;
+
+      object = window.document.createElement('object');
+      for (i = 0; i < objectAttributes.length; i++) {
+        object.setAttribute(objectAttributes[i][0], objectAttributes[i][1]);
       }
       
-      var params = [
-        ['movie', '/js/clippy.swf'],
-        ['allowScriptAccess', 'always'],
-        ['quality', 'high'],
-        ['scale', 'noscale'],
-        ['FlashVars', 'text='+config.data],
-        ['bgcolor', 'transparent'],
-        ['wmode', 'transparent']
-      ];
-      for (var i = 0; i < params.length; i++) {
-        var param = window.document.createElement('param');
-        param.name = params[i][0];
-        param.value = params[i][1];
+      for (i = 0; i < params.length; i++) {
+        param = window.document.createElement('param');
+        param.setAttribute('name', params[i][0]);
+        param.setAttribute('value', params[i][1]);
         object.appendChild(param);
       }
 
-      var embedAttributes = [
-        ['src', '/js/clippy.swf'],
-        ['width', width],
-        ['height', height],
-        ['name', 'clippy'],
-        ['quality', 'high'],
-        ['allowScriptAccess', 'always'],
-        ['type', 'application/x-shockwave-flash'],
-        ['pluginspage', 'http://www.macromedia.com/go/getflashplayer'],
-        ['FlashVars', 'text='+config.data],
-        ['bgcolor', 'transparent'],
-        ['wmode', 'transparent']
-      ];
-      var embed = window.document.createElement('embed');
-      for (var i = 0; i < embedAttributes.length; i++) {
-        var key = embedAttributes[i][0];
-        var value = embedAttributes[i][1];
-        embed.setAttribute(key, value);
+      embed = window.document.createElement('embed');
+      for (i = 0; i < embedAttributes.length; i++) {
+        embed.setAttribute(embedAttributes[i][0], embedAttributes[i][1]);
       }
-      object.appendChild(embed);
 
+      object.appendChild(embed);
       return object;
     }.bind(this);
     
